9f051a2626ed9cc8dbd37dbcfa2e7bd6f3ab6430
lerry <lerryletter@gmail.com>
2017-06-27 11:22:47 +0800

    添加看书笔记

---
+++ JSbook/script_ele.js
@@ -0,0 +1,16 @@
+/**
+ * Created by lerry on 27/06/2017.
+ * 基本概念
+ */
+
+// 简单数据类型 Undefined,Null,Boolean,Number,String.
+//复杂数据类型Object
+/**
+ *Undefined 只有一个值 undefined
+ */
+// start:for (var i = 10; i < 10; i++) {
+//     continue start
+// }
+with (location) {
+    search.substring(1)
+}
